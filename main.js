bf46b287e13e6c0d56fdedcee984c4f07a19a569
mahaboobpasha115 <mahaboobpashapasha115@gmail.com>
2020-12-31 19:08:04 +0530

    image uploader created

---
+++ main.js
@@ -0,0 +1,17 @@
+
+window.addEventListener('load', function(){
+    document.querySelector('input[type="file"]').addEventListener('change',function(){
+        if (this.files && this.files[0]){
+            var img= document.querySelector('img');
+            img.src = URL.createObjectURL(this.files[0]);
+            img.onload= imageIsLoaded;
+
+
+        }
+    });
+});
+   
+   function imageIsLoaded(e){
+       alert("Image is Uploaded");
+   }
+
